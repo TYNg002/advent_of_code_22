d205a23242c458cc9a650b20d767176925597665
Tsing Yu Ng <ngtyresha@hotmail.com>
2023-03-06 16:31:17 +0000

    New files

---
+++ 2206/2206_solution1.py
@@ -0,0 +1,30 @@
+with open("2206_input.txt") as inputfile:
+    input = inputfile.read()
+
+# store characters to be assessed
+assess = []
+
+for num, char in enumerate(input, 1):
+    if num < 4:
+        assess.append(char)
+    else:
+        assess.append(char)
+        repeat = False
+        for item in assess:
+            # count occurrences of item in assess
+            counter = 0
+            for item2 in assess:
+                if item == item2:
+                    counter += 1
+                # if counter is greater than 1, it means a repeat exists
+                if counter > 1:
+                    repeat = True
+                    break
+            # exit loop if repeat is detected
+            if repeat == True:
+                break
+        # if all items have been iterated through and no repeat exists in assess
+        if repeat == False:
+            print(num)
+            exit()
+        assess.pop(0)
